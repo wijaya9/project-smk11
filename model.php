133339d53c21a7fdcff2af64568062ae78bc8833
Fatoni <fatoniahmad.mail@gmail.com>
2021-08-24 11:20:45 +0700

    review-24-agustus

--- model.php
+++ model.php
@@ -27,36 +27,40 @@
                 $baris[] = $obj;
             }
 
+            // langsung return $baris saja, tanpa if
+
             if(!empty($baris))
             {
                 return $baris;
             }
         }
 
-        public function find()
+        public function find($id) // perlu di sesuaikan di class yg memanggil function ini
         {
-            $sql = "Select * from siswa where id='$this->id'";
+            $sql = "Select * from siswa where id='$id'";
             $bind = $this->conn->query($sql);
             while ($obj = $bind->fetch_object())
             {
                 $baris = $obj;
             }
 
+            // langsung return $baris saja, tanpa if
+
             if(!empty($baris))
             {
                 return $baris;
             }
         }
 
-        public function update()
+        public function update($id)
         {
-            $sql = "update siswa set nama='$this->nama', nis='$this->nis', alamat='$this->alamat', jk='$this->jk', rombel='$this->rombel' where id='$this->id'";
+            $sql = "update siswa set nama='$this->nama', nis='$this->nis', alamat='$this->alamat', jk='$this->jk', rombel='$this->rombel' where id='$id'";
             $this->conn->query($sql);
         }
 
-        public function delete()
+        public function delete($id)
         {
-            $sql = "Delete from siswa where id='$this->id'";
+            $sql = "Delete from siswa where id='$id'";
             $this->conn->query($sql);
         }
 
